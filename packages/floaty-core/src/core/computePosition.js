e9ab44f08dda14228d0c7ae2e1490a61b4d196b7
seo-ny <seony0301@gmail.com>
2025-04-27 23:43:06 +0900

    feat: computePosition에서 유틸 함수, 변수 사용 (options 정의, element rect 구함)

--- packages/floaty-core/src/core/computePosition.js
+++ packages/floaty-core/src/core/computePosition.js
@@ -1,1 +1,24 @@
-export const computePosition = () => {};
+import { DEFAULT_OPTIONS } from "@/constants/index.js";
+import { rectUtils, validateUtils } from "@/utils/index.js";
+
+export const computePosition = (
+  referenceEl = null,
+  floatingEl = null,
+  originOptions = {}
+) => {
+  if (
+    !validateUtils.isHTMLElement(referenceEl) ||
+    !validateUtils.isHTMLElement(floatingEl)
+  )
+    return;
+
+  const options = { ...DEFAULT_OPTIONS, ...originOptions };
+  const referenceElRect = rectUtils.getElementRect(referenceEl);
+  const floatingElRect = rectUtils.getElementRect(floatingEl);
+
+  console.log("[computePosition]", {
+    referenceElRect,
+    floatingElRect,
+    options
+  });
+};
