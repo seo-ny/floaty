79e54e90b67ffb8642ce14a2dd4fa1fe89dc8ed1
seo-ny <seony0301@gmail.com>
2025-05-02 23:45:20 +0900

    chore: vanilla playground에서 story 전역 스타일링 래퍼 추가

--- playground/vanilla/.storybook/preview.js
+++ playground/vanilla/.storybook/preview.js
@@ -13,3 +13,19 @@ export const parameters = {
   },
   layout: "fullscreen" // storybook 자체적 body padding style 없애기 위한 옵션
 };
+
+export const decorators = [
+  (StoryFn) => {
+    const wrapper = document.createElement("div");
+    wrapper.style.display = "flex";
+    wrapper.style.justifyContent = "center";
+    wrapper.style.alignItems = "center";
+    wrapper.style.width = "100%";
+    wrapper.style.height = "100vh";
+
+    const story = StoryFn();
+    wrapper.appendChild(story);
+
+    return wrapper;
+  }
+];
